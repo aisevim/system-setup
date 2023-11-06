627dc0b1500c121f0f9fc5f5aa7967ff020a3734
aisevim <a.aydnlp.pro@gmail.com>
2023-11-06 23:06:32 +0100

    fix(curl): change wording

--- bin/install_curl.sh
+++ bin/install_curl.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if HEL::command_not_exist "curl"; then
+	echoIBlue "install 'curl'..."
+
 	# https://curl.se/
   sudo apt install curl
 
 	if ! HEL::command_not_exist "curl"; then
-	  echoIGreen "curl install success"
+	  echoIGreen "'curl' install success"
 	else
-	  echoOnIRed "curl install failed :("
+	  echoOnIRed "'curl' install failed :("
 		exit 1
 	fi
 fi
