1daf10ab0bb2d63a57fa2c78f57abff3ce7ed608
aisevim <a.aydnlp.pro@gmail.com>
2023-11-07 01:35:52 +0100

    fix(curl): delete check curl + wording

--- bin/install_volta_node_pnpm.sh
+++ bin/install_volta_node_pnpm.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-if HEL::command_not_exist "curl"; then
-	echoIBlue "install 'volta'..."
+echoIBlue "install 'volta'..."
 
-	# https://volta.sh/
-	# node version manager
-  curl https://get.volta.sh | bash
+# https://volta.sh/
+# node version manager
+curl https://get.volta.sh | bash
 
-	echoIBlue "install node 18"
-	volta install node@18
-	volta install pnpm
+echoIBlue "install node 18"
+volta install node@18
+volta install pnpm
 
-	if ! HEL::command_not_exist "pnpm" && ! HEL::command_not_exist "node"; then
-		echoIGreen "'node' and 'pnpm' install success"
-	else
-		echoOnIRed "'pnpm' install failed :("
-		exit 1
-	fi
+if ! HEL::command_not_exist "pnpm" && ! HEL::command_not_exist "node"; then
+	echoIGreen "'node' and 'pnpm' install success"
+else
+	echoOnIRed "'node' or 'pnpm' install failed :("
+	exit 1
 fi
