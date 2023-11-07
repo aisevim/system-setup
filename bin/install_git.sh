fe9868a637930dce0a3ef1cb2e4d973925ed73f4
aisevim <a.aydnlp.pro@gmail.com>
2023-11-07 01:35:17 +0100

    fix(git): check git command exist

--- bin/install_git.sh
+++ bin/install_git.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echoIBlue "copy git config files..."
+if HEL::command_not_exist "git"; then
+	echoIBlue "install 'git'..."
+	sudo apt install git-all
+	echoIGreen "'git' install success"
+fi
 
+echoIBlue "copy git config files..."
 cp ./data/git/.gitconfig ~/.gitconfig
 cp -r ./data/git/.git-custom ~/.git-custom
-
 echoIGreen "copy done"
